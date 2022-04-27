34410563f732da651e09bc6bbeab80d3277fa69b
sm185592 <sm185592@ncr.com>
2022-04-27 13:44:46 +0100

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,80 @@
+-- Plugin Management
+require "plugins"
+
+-- Plugin Config Files
+require "configs/nvim-tree"
+require "configs/treesitter"
+
+-- Settings
+local set = vim.opt
+
+-- STYLE
+
+-- Window Title
+set.titlestring = "NeoVim"
+set.title = true
+
+-- Command Line
+set.cmdheight = 1
+
+-- EDITOR
+
+-- Line Numbers
+set.number = true
+set.relativenumber = true
+
+-- TabWidth
+set.shiftwidth = 4
+set.showtabline = 1
+
+-- SEARCH FUNCTIONS
+
+-- Search Highlighting
+set.hlsearch = true
+
+-- Incremental Search
+set.incsearch = true
+
+-- Case Insensitivity
+set.ignorecase = true
+
+-- Smart Case Sensitivity
+set.smartcase = true
+
+-- KEYMAP
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("", "<Space>", "<Nop>", {})
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Moving between splits with Ctrl + [HJKL]
+keymap("n", "<A-h>", "<C-w>h", {})
+keymap("n", "<A-j>", "<C-w>j", {})
+keymap("n", "<A-k>", "<C-w>k", {})
+keymap("n", "<A-l>", "<C-w>l", {})
+
+-- Resizing splits with Alt + [HJKL]
+keymap ("n", "<C-h>", ":vertical resize -5<cr>", {})
+keymap ("n", "<C-j>", ":resize +5<cr>", {})
+keymap ("n", "<C-k>", ":resize -5<cr>", {})
+keymap ("n", "<C-l>", ":vertical resize +5<cr>", {})
+
+
+-- Auto Pairs
+vim.cmd([[
+inoremap ( ()<left>
+inoremap [ []<left>
+inoremap { {}<left>
+inoremap {<CR> {<CR>}<ESC>0
+inoremap {;<CR> {<CR>};<ESC>0
+nnoremap <Leader>o o<Esc>^Da
+nnoremap <Leader>O O<Esc>^Da
+inoremap <expr><TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
+inoremap " ""<left>
+inoremap ' ''<left>
+]])
+
+
+  
---
+++ lua/plugins.lua
@@ -0,0 +1,74 @@
+vim.cmd[[packadd packer.nvim]]
+
+-- Intialise Packer
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Use Packer in a pop up
+packer.init {
+	display = {
+		open_fn = function()
+			return require("packer.util").float { border = "rounded" }
+		end,
+	},
+}
+  
+-- Install Plugins
+return packer.startup(function()
+	-- Package Manager
+	use 'wbthomason/packer.nvim'
+
+	-- File Tree
+	use {
+		'kyazdani42/nvim-tree.lua',
+    		requires = {
+    			'kyazdani42/nvim-web-devicons'
+    		}, 
+		cmd = {"NvimTreeToggle", "NvimTreeClose"}, 
+			config = function()
+				require("nvim-tree").setup {
+				    update_focused_file = {enable = true, update_cwd = true}
+				}
+			end,
+	}
+
+	-- Tree Sitter
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+
+	use {
+		'goolord/alpha-nvim',
+		config = function()
+			require'alpha'.setup(require'alpha.themes.dashboard'.config)
+		end
+	}
+
+	use {
+		'RRethy/nvim-base16',
+		config = function()
+			vim.cmd('colorscheme base16-tomorrow-night')
+		end
+	}
+
+	use {
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require('lualine').setup{
+				options = { theme = 'base16'}
+			}
+		end
+	}
+
+	use {
+		'lewis6991/gitsigns.nvim',
+		config = function()
+		  	require('gitsigns').setup()
+		end
+	}
+
+	use "lewis6991/impatient.nvim"
+end)
