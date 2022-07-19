1994a99d2b1a33364dace03a439782e2d074eb12
sm185592 <sm185592@ncr.com>
2022-07-19 13:43:55 +0100

    Added minimum lsp configurations for a handful of languages

--- lua/configs/completion.lua
+++ lua/configs/completion.lua
@@ -128,12 +128,52 @@ require("nvim-lsp-installer").setup({
 -- LSP servers
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
+-- clangd
+require('lspconfig').clangd.setup{
+    capabilities = capabilities
+}
+
+-- csharp_ls 
+require('lspconfig').csharp_ls.setup{
+    capabilities = capabilities
+}
+
+-- gopls
+require('lspconfig').gopls.setup{
+    capabilities = capabilities
+}
+
+-- hls
+require('lspconfig').hls.setup{
+    capabilities = capabilities
+}
+
+-- julials
+require('lspconfig').julials.setup{
+    capabilities = capabilities
+}
+
 -- pyright
 require("lspconfig").pyright.setup{
     capabilities = capabilities
 }
 
+-- rust analyzer
+require('lspconfig').rust_analyzer.setup{
+    capabilities = capabilities
+}
+
 -- sumneko lua 
 require('lspconfig').sumneko_lua.setup{
+    capabilities = capabilities
+}
+
+-- taplo
+require('lspconfig').taplo.setup{
+    capabilities = capabilities
+}
+
+-- yamlls 
+require('lspconfig').yamlls.setup{
     capabilities = capabilities
 }
