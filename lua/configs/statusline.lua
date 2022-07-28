e5f31f885f016a6cd5c314173c7c224c09c8db89
sm185592 <sm185592@ncr.com>
2022-07-28 17:13:58 +0100

    Added diagnostics to heirline config

--- lua/configs/statusline.lua
+++ lua/configs/statusline.lua
@@ -98,7 +98,7 @@ local WorkDir = {
 	local trail = cwd:sub(-1) == '/' and ' ' or "/ "
 	return icon .. cwd .. trail
     end,
-    hl = { fg = "#f9e2af", bg = "#313244", bold = false },
+    hl = { fg = "#a6adc8", bg = "#313244", bold = false },
 }
 
 -- Basic Ruler
@@ -107,9 +107,67 @@ local Ruler = {
     hl = { fg ="#a6adc8", bg = "#313244"}
 }
 
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+local Diagnostics = {
+    consdition = conditions.has_diagnostics,
+
+    static = {
+	error_icon = vim.fn.sign_getdefined("DiagnosticSignError")[1].text,
+	warn_icon = vim.fn.sign_getdefined("DiagnosticSignWarn")[1].text,
+	info_icon = vim.fn.sign_getdefined("DiagnosticSignInfo")[1].text,
+	hint_icon = vim.fn.sign_getdefined("DiagnosticSignHint")[1].text,
+    },
+
+    init = function (self)
+    	self.errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR })
+    	self.warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN })
+    	self.hints = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.HINT })
+    	self.info = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.INFO })
+    end,
+
+    update = { "DiagnosticChanged", "BufEnter" },
+
+    {
+	provider = " ",
+	hl = { bg = "#45475a" },
+    },
+    {
+	provider = function (self)
+	    return self.errors > 0 and (self.error_icon .. self.errors .. " ")
+	end,
+	hl = { fg = "#f38ba8", bg = "#45475a" },
+    },
+    {
+	provider = function (self)
+	    return self.warnings > 0 and (self.warn_icon .. self.warnings .. " ")
+	end,
+	hl = { fg = "#f9e2af", bg = "#45475a" },
+    },
+    {
+	provider = function (self)
+	    return self.info > 0 and (self.info_icon .. self.info .. " ")
+	end,
+	hl = { fg = "#89b4fa", bg = "#45475a" },
+    },
+    {
+	provider = function (self)
+	    return self.hints > 0 and (self.hint_icon .. self.hints)
+	end,
+	hl = { fg = "#94e2d5", bg = "#45475a" },
+    },
+    {
+	provider = " ",
+	hl = { bg = "#45475a" },
+    },
+}
+
 -- Build out the status line
 local statusline = {
-    ViMode, WorkDir, { provider = "%=" }, Ruler
+    ViMode, WorkDir, Diagnostics, { provider = "%=" }, Ruler
 }
 
 require'heirline'.setup(statusline)
