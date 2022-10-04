f2692e9152e0a6c15ce8f4835eb33113d1c66dfa
sm185592 <sm185592@ncr.com>
2022-10-04 16:58:29 +0100

    Modified Noice's Cmdline Popup

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -1,2 +1,44 @@
-require("noice").setup()
+local noice_status, noice = pcall(require, 'noice')
+if not noice_status then
+  return
+end
+
+noice.setup({
+	views = {
+		cmdline_popup = {
+			position = {
+				row = 5,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = "auto",
+			},
+			border = {
+				style = "single",
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "Normal" }
+			}
+		},
+		popupmenu = {
+			relative = "editor",
+			position = {
+				row = 8,
+				col = "50%",
+			},
+			size = {
+				width = 60,
+				height = 10,
+			},
+			border = {
+				style = "single",
+				padding = { 0, 1 },
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" }
+			}
+		}
+	}
+})
 
