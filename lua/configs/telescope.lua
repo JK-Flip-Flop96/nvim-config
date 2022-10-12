f9ae93efcc99ebfaa408cb4c5f0f2d9fe04c7492
sm185592 <sm185592@ncr.com>
2022-10-12 14:26:56 +0100

    Square borders on telescope windows

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -8,3 +8,9 @@ vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+telescope.setup({
+	defaults = {
+		borderchars = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+	}
+})
