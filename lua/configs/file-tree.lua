69843abf54acff1e3ad2fe215d42d1f69db1f50b
sm185592 <sm185592@ncr.com>
2022-11-01 16:28:39 +0000

    Added custom nvim-tree config

--- lua/configs/file-tree.lua
+++ lua/configs/file-tree.lua
@@ -4,4 +4,28 @@ if not file_tree_ok then
   return
 end
 
-nvim_tree.setup()
+nvim_tree.setup({
+	renderer = {
+		indent_markers = {
+			enable = true,
+			inline_arrows = true,
+		},
+		icons = {
+			git_placement = "signcolumn",
+			show = {
+				folder_arrow = false,
+			},
+			glyphs = {
+				git = {
+					unstaged = "",
+					staged = "",
+					unmerged = "",
+					renamed = "",
+					untracked = "",
+					deleted = "",
+					ignored = "",
+				}
+			}
+		}
+	}
+})
