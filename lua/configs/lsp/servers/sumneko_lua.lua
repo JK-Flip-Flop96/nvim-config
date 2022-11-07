74323ab04fa7cc3c0ed919ac030c1f611c9674e7
sm185592 <sm185592@ncr.com>
2022-11-07 13:37:09 +0000

    feat(lsp): Updated sumneko_lua config

--- lua/configs/lsp/servers/sumneko_lua.lua
+++ lua/configs/lsp/servers/sumneko_lua.lua
@@ -7,14 +7,27 @@ require('lspconfig').sumneko_lua.setup{
     capabilities = capabilities,
     settings = {
 		Lua = {
+			-- LuaJIT in the case of Neovim
+			runtime = {
+				version = 'LuaJIT',
+			},
 	    	-- Ensure that the LSP server recognises the 'vim' global 
 	    	-- and Packer's 'use' function
 	    	diagnostics = {
 				globals = { 'vim', 'use' }
 			},
-		}
+			workspace = {
+				-- Get the language server to recognise Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				-- Disable telemetry
+				enable = false,
+			},
+		},
     },
     on_attach = function(client, bufnr)
+		-- Attach Navic
 		navic.attach(client, bufnr)
     end,
 }
